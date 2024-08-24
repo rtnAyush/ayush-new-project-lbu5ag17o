5ba3f7eb262bd525cf62ade9c93f645fe173492e
Abhilasha <singhabhilasha4042@gmail.com>
2024-08-24 13:24:02 +0530

    fix login and layout page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,13 @@
-import { auth, signIn } from '@/lib/auth';
 import React from 'react'
 
-export const LoginPage = async () => {
+const LoginPage = async () => {
 
-    const session = await auth();
-    console.log(session);
 
   return (
     <div>
-        <form action={handleGithubLogin}>
-            <button onClick={handleGithubLogin}>Login with Github</button>
-        </form>
+     
     </div>
   )
 }
+
+export default LoginPage;
