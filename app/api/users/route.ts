08808107177f307f3afc4f9110e77e5f31fd14b3
Abhilasha <singhabhilasha4042@gmail.com>
2024-08-31 09:10:59 +0530

    removes src dir

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,17 +3,17 @@ import prisma from '../../../lib/prisma';
 
 // GET method to fetch all users
 export async function GET() {
-  const users = await prisma.user.findMany();
+  const users = await prisma.users.findMany();
   return NextResponse.json(users);
 }
 
 // POST method to create a new user
 export async function POST(request: Request) {
   const { name, email } = await request.json();
-  const newUser = await prisma.user.create({
+  const newUser = await prisma.users.create({
     data: {
-      name,
       email,
+      password: 'password',
     },
   });
   return NextResponse.json(newUser, { status: 201 });
