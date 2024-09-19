a35b12c7ee91f8b0a283dfaecfc12dfcd72b9903
Abhilasha <singhabhilasha4042@gmail.com>
2024-09-19 09:46:34 +0530

    fix routes

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -8,40 +8,5 @@ type LoginRequestBody = {
 }
 
 export async function POST(request: Request) {
-  try {
-    const body: LoginRequestBody = await request.json()
-    const { email, password } = body
-
-    if (!email || !password) {
-      return NextResponse.json({ error: 'Email and password are required' }, { status: 400 })
-    }
-
-    // Find the user by email
-    const user = await prisma.users.findUnique({ where: { email } })
-    
-    if (!user) {
-      return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
-    }
-
-    // Compare the provided password with the stored hash
-    const isPasswordValid = user.password == password;
-    
-    if (!isPasswordValid) {
-      return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 })
-    }
-
-    // Generate a JWT token
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      process.env.JWT_SECRET || 'secret',
-      { expiresIn: '10h' } // Token expires in 1 hour
-    )
-
-    // Return the token
-    console.log('User logged in:', { token, user: user })
-    return NextResponse.json({ token, user: user }, { status: 200 })
-  } catch (error) {
-    console.error('Login error:', error)
-    return NextResponse.json({ error: 'An error occurred during login' }, { status: 500 })
-  }
+  
 }
