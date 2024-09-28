9f27b80d4bb43bc4a51285bab7a5cfd2d9f09654
Abhilasha <singhabhilasha4042@gmail.com>
2024-09-28 16:44:37 +0530

    fix

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -3,14 +3,14 @@ import prisma from '../../../lib/prisma';
 
 // GET method to fetch all users
 export async function GET() {
-  const users = await prisma.users.findMany();
+  const users = await prisma.user.findMany();
   return NextResponse.json(users);
 }
 
 // POST method to create a new user
 export async function POST(request: Request) {
   const { name, email } = await request.json();
-  const newUser = await prisma.users.create({
+  const newUser = await prisma.user.create({
     data: {
       name: name,
       email,
