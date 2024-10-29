be06377ef3cccfbeeeed9c143678d41ce077bb40
Abhilasha <singhabhilasha4042@gmail.com>
2024-10-29 12:51:52 +0530

    adds wobble card

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.prod.website-files.com','www.branchfurniture.ca', 'smiletwice.com', 'www.pngall.com', 'randomuser.me', 'picsum.photos'],
+        domains: ['cdn.prod.website-files.com','www.branchfurniture.ca', 'smiletwice.com', 'www.pngall.com', 'dalleproduse.blob.core.windows.net', 'randomuser.me', 'picsum.photos', 'images.unsplash.com', 'cdn.midjourney.com'],
     }
 };
 
