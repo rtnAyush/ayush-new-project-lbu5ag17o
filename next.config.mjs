76e81875443833a87efd80e17a5d4ce5b2b9b35b
Abhilasha <singhabhilasha4042@gmail.com>
2024-10-29 11:20:43 +0530

    adds the image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.prod.website-files.com'],
+        domains: ['cdn.prod.website-files.com','www.branchfurniture.ca', 'smiletwice.com', 'www.pngall.com', 'randomuser.me', 'picsum.photos'],
     }
 };
 
