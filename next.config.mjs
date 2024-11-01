4dcc0ba859409f5406292ab4b717d66b7a9c5fc2
Abhilasha <singhabhilasha4042@gmail.com>
2024-11-01 16:49:18 +0530

    adds image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['cdn.prod.website-files.com','www.branchfurniture.ca', 'smiletwice.com', 'www.pngall.com', 'dalleproduse.blob.core.windows.net', 'media.istockphoto.com', 'randomuser.me', 'picsum.photos', 'images.unsplash.com', 'cdn.midjourney.com', 'picsum.photos'],
+        domains: ['fastly.picsum.photo','cdn.prod.website-files.com','www.branchfurniture.ca', 'smiletwice.com', 'www.pngall.com', 'dalleproduse.blob.core.windows.net', 'media.istockphoto.com', 'randomuser.me', 'picsum.photos', 'images.unsplash.com', 'cdn.midjourney.com', 'picsum.photos'],
     }
 };
 
