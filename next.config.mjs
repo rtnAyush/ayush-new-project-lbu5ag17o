66527bb621e3671e36d330ec50b8bcab724f3535
Abhilasha <singhabhilasha4042@gmail.com>
2024-11-01 16:50:10 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['fastly.picsum.photo','cdn.prod.website-files.com','www.branchfurniture.ca', 'smiletwice.com', 'www.pngall.com', 'dalleproduse.blob.core.windows.net', 'media.istockphoto.com', 'randomuser.me', 'picsum.photos', 'images.unsplash.com', 'cdn.midjourney.com', 'picsum.photos'],
+        domains: ['fastly.picsum.photos','cdn.prod.website-files.com','www.branchfurniture.ca', 'smiletwice.com', 'www.pngall.com', 'dalleproduse.blob.core.windows.net', 'media.istockphoto.com', 'randomuser.me', 'picsum.photos', 'images.unsplash.com', 'cdn.midjourney.com', 'picsum.photos'],
     }
 };
 
